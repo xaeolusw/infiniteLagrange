66d4fca96080d7225a8f9b7b611401d80eed54f5
Jinhui Xie <xaeolus0769@gmail.com>
2023-09-20 16:03:27 +0800

    modified:   test/test.py

--- test/test.py
+++ test/test.py
@@ -1,75 +1,32 @@
-import sys
+import random
 import time
 
-SIZE = 5
-total = 0
+
+def record_time(func):
+
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f'{func.__name__}执行时间: {end - start:.3f}秒')
+        return result
+
+    return wrapper
 
 
-def print_board(board):
-    for row in board:
-        for col in row:
-            print(str(col).center(4), end='')
-        print()
+@record_time
+def download(filename):
+    print(f'开始下载{filename}.')
+    time.sleep(random.randint(2, 6))
+    print(f'{filename}下载完成.')
 
 
-def patrol(board, row, col, step=1):
-    if row >= 0 and row < SIZE and \
-        col >= 0 and col < SIZE and \
-        board[row][col] == 0:
-        board[row][col] = step
-        if step == SIZE * SIZE:
-            global total
-            total += 1
-            print(f'第{total}种走法: ')
-            # print_board(board)
-            test_pos (board, row, col)
-        patrol(board, row - 2, col - 1, step + 1)
-        patrol(board, row - 1, col - 2, step + 1)
-        patrol(board, row + 1, col - 2, step + 1)
-        patrol(board, row + 2, col - 1, step + 1)
-        patrol(board, row + 2, col + 1, step + 1)
-        patrol(board, row + 1, col + 2, step + 1)
-        patrol(board, row - 1, col + 2, step + 1)
-        patrol(board, row - 2, col + 1, step + 1)
-        board[row][col] = 0
-
-def test_pos (board, row, col):
-    if row - 2 >= 0 and row - 2 < SIZE :
-       if col - 1 >=0 and col - 1 < SIZE:
-           if board[row - 2][col - 1] == 1:
-               print_board(board)
-       elif col + 1 >= 0 and col + 1 < SIZE:
-           if board[row - 2][col + 1] == 1:
-               print_board(board)
-    elif row - 1 >= 0 and row - 1 < SIZE :
-       if col - 2 >=0 and col - 2 < SIZE:
-           if board[row - 1][col - 2] == 1:
-               print_board(board)
-       elif col + 2 >= 0 and col + 2 < SIZE:
-           if board[row - 1][col + 2] == 1:
-               print_board(board)
-    elif row + 1 >= 0 and row + 1 < SIZE :
-       if col - 2 >=0 and col - 2 < SIZE:
-           if board[row + 1][col - 2] == 1:
-               print_board(board)
-       elif col + 2 >= 0 and col + 2 < SIZE:
-           if board[row + 1][col + 2] == 1:
-               print_board(board)
-    elif row + 2 >= 0 and row + 2 < SIZE :
-       if col - 1 >=0 and col - 1 < SIZE:
-           if board[row + 2][col - 1] == 1:
-               print_board(board)
-       elif col + 1 >= 0 and col + 1 < SIZE:
-           if board[row + 2][col + 1] == 1:
-               print_board(board)
-
-  
-def main():
-    board = [[0] * SIZE for _ in range(SIZE)]
-    for i in range(SIZE):
-        for j in range(SIZE):
-            patrol(board, i, j)
+@record_time
+def upload(filename):
+    print(f'开始上传{filename}.')
+    time.sleep(random.randint(4, 8))
+    print(f'{filename}上传完成.')
 
 
-if __name__ == '__main__':
-    main()
+download('MySQL从删库到跑路.avi')
+upload('Python从入门到住院.pdf')
