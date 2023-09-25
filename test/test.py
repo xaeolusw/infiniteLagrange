388f12b45ba2ac7ddff31f4a6acfe7e6dd80e793
Jinhui Xie <xaeolus0769@gmail.com>
2023-09-25 17:20:15 +0800

    modified:   test/test.py

--- test/test.py
+++ test/test.py
@@ -1,50 +1,10 @@
-from enum import Enum
-import random
-class Suite(Enum):
-    """花色(枚举)"""
-    SPADE, HEART, CLUB, DIAMOND = range(4)
+import re
+import requests
 
-class Card:
-    """牌"""
-
-    def __init__(self, suite, face):
-        self.suite = suite
-        self.face = face
-
-    def __repr__(self):
-        suites = '♠♥♣♦'
-        faces = ['', 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-        # 根据牌的花色和点数取到对应的字符
-        return f'{suites[self.suite.value]}{faces[self.face]}'
-
-class Poker:
-    """扑克"""
-
-    def __init__(self):
-        # 通过列表的生成式语法创建一个装52张牌的列表
-        self.cards = [Card(suite, face) for suite in Suite
-                      for face in range(1, 14)]
-        # current属性表示发牌的位置
-        self.current = 0
-
-    def shuffle(self):
-        """洗牌"""
-        self.current = 0
-        # 通过random模块的shuffle函数实现列表的随机乱序
-        random.shuffle(self.cards)
-
-    def deal(self):
-        """发牌"""
-        card = self.cards[self.current]
-        self.current += 1
-        return card
-
-    @property
-    def has_next(self):
-        """还有没有牌可以发"""
-        return self.current < len(self.cards)
-    
-poker = Poker()
-print(poker.cards)
-poker.shuffle()
-print(poker.cards)
+pattern = re.compile(r'<a.*?href="(.*?)".*?title="(.*?)".*?>')
+resp = requests.get('https://www.sohu.com/')
+if resp.status_code == 200:
+    all_matches = pattern.findall(resp.text)
+    for href, title in all_matches:
+        print(href)
+        print(title)
