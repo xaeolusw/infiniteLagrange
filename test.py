660f62cd0f675bda0162e593f7b30db15eb2a074
Jinhui Xie <xaeolus0769@gmail.com>
2023-10-25 08:31:43 +0800

    20231025

--- test.py
+++ test.py
@@ -1,10 +1,26 @@
-from collections import Counter
+import concurrent.futures
+import urllib.request
 
-words = [
-  'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
-  'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around',
-  'the', 'eyes', "don't", 'look', 'around', 'the', 'eyes',
-  'look', 'into', 'my', 'eyes', "you're", 'under'
-]
-counter = Counter(words)
-print(counter.most_common(3))
+URLS = ['http://www.foxnews.com/',
+        'http://www.cnn.com/',
+        'http://europe.wsj.com/',
+        'http://www.bbc.co.uk/',
+        'http://nonexistant-subdomain.python.org/']
+
+# Retrieve a single page and report the URL and contents
+def load_url(url, timeout):
+    with urllib.request.urlopen(url, timeout=timeout) as conn:
+        return conn.read()
+
+# We can use a with statement to ensure threads are cleaned up promptly
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    # Start the load operations and mark each future with its URL
+    future_to_url = {executor.submit(load_url, url, 60): url for url in URLS}
+    for future in concurrent.futures.as_completed(future_to_url):
+        url = future_to_url[future]
+        try:
+            data = future.result()
+        except Exception as exc:
+            print('%r generated an exception: %s' % (url, exc))
+        else:
+            print('%r page is %d bytes' % (url, len(data)))
